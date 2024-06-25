c5e4ad6fd6653788f1f6e4f313cb9268ba625ea5
叶浩 <77729052@qq.com>
2024-06-25 15:57:41 +0800

    增加 websocket 模块

--- seal/wrapper/__init__.py
+++ seal/wrapper/__init__.py
@@ -1,3 +1,4 @@
 from ._entity import entity
 from ._singleton import singleton
 from ._cache import cache_func
+from ._ws_handler import handler
---
+++ seal/ws/__init__.py
@@ -0,0 +1,2 @@
+from ._ws import start_background
+from ._ws_dispatcher import dispatcher
