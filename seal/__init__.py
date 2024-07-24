f61e845d51239f20ba26743b9975fc529d5dc83b
叶浩 <77729052@qq.com>
2024-07-24 17:32:38 +0800

    feat: 新增 cache 模块

--- seal/__init__.py
+++ seal/__init__.py
@@ -2,6 +2,6 @@ from .seal import Seal as _Seal
 
 __author__ = 'melon'
 __email__ = '77729052@qq.com'
-__version__ = '0.3.5'
+__version__ = '0.4.0'
 
 seal: _Seal = _Seal()
--- seal/seal.py
+++ seal/seal.py
@@ -3,21 +3,23 @@ from .db.query_wrapper import QueryWrapper
 from .db.update_wrapper import UpdateWrapper
 from .db.insert_wrapper import InsertWrapper
 from .db.wrapper import Wrapper
+from .cache import LRUCache
 from loguru import logger
 
 
 class Seal:
 
     def __init__(self):
-        self.configuration = Configuration()
-        self.initialized = False
-        self.data_sources = {}
-        self.default_data_source = None
-        self.logical_delete = None
+        self._configuration = Configuration()
+        self._initialized = False
+        self._data_sources = {}
+        self._default_data_source = None
+        self._logical_delete = None
+        self._default_cache: LRUCache = LRUCache(512)
 
     def init(self, config_path):
-        self.configuration.load(config_path)
-        self.initialized = True
+        self._configuration.load(config_path)
+        self._initialized = True
 
         logger.add(self.get_config('seal', 'loguru', 'path'),
                    rotation=self.get_config('seal', 'loguru', 'rotation'),
@@ -29,60 +31,63 @@ class Seal:
             data_source = self.get_config('seal', 'data_source', data_source_name)
             if 'mysql' == data_source.get('type'):
                 from .db.mysql.mysql_data_source import MysqlDataSource
-                self.data_sources[data_source_name] = MysqlDataSource(data_source_conf)
+                self._data_sources[data_source_name] = MysqlDataSource(data_source_conf)
             elif 'sqlite' == data_source.get('type'):
                 from .db.sqlite.sqlite_data_source import SqliteDataSource
-                self.data_sources[data_source_name] = SqliteDataSource(data_source_conf)
+                self._data_sources[data_source_name] = SqliteDataSource(data_source_conf)
             else:
                 raise ValueError(f'不支持的数据源类型: {data_source.get("type")}')
 
             if data_source_conf.get('default', False):
-                self.default_data_source = self.data_sources[data_source_name]
+                self._default_data_source = self._data_sources[data_source_name]
 
-        if len(self.data_sources.values()) == 1 and self.default_data_source is None:
-            self.default_data_source = list(self.data_sources.values())[0]
-        if self.default_data_source is None:
+        if len(self._data_sources.values()) == 1 and self._default_data_source is None:
+            self._default_data_source = list(self._data_sources.values())[0]
+        if self._default_data_source is None:
             raise ValueError('未配置默认数据源')
         logger.info(f'初始化 seal({self}) 成功')
         return self
 
     def data_source(self, data_source_name):
-        if not self.initialized:
+        if not self._initialized:
             raise ValueError('Seal 未初始化')
-        return self.data_sources.get(data_source_name)
+        return self._data_sources.get(data_source_name)
 
     def get_config(self, *keys):
-        if not self.initialized:
+        if not self._initialized:
             raise ValueError('Seal 未初始化')
-        return self.configuration.get_conf(*keys)
+        return self._configuration.get_conf(*keys)
 
     def q(self, table):
-        return QueryWrapper(table, self.default_data_source, self.logical_delete)
+        return QueryWrapper(table, self._default_data_source, self._logical_delete)
 
     def query_wrapper(self, table):
-        return QueryWrapper(table, self.default_data_source, self.logical_delete)
+        return QueryWrapper(table, self._default_data_source, self._logical_delete)
 
     def u(self, table):
-        return UpdateWrapper(table, self.default_data_source, self.logical_delete)
+        return UpdateWrapper(table, self._default_data_source, self._logical_delete)
 
     def update_wrapper(self, table):
-        return UpdateWrapper(table, self.default_data_source, self.logical_delete)
+        return UpdateWrapper(table, self._default_data_source, self._logical_delete)
 
     def i(self, table):
-        return InsertWrapper(table, self.default_data_source)
+        return InsertWrapper(table, self._default_data_source)
 
     def insert_wrapper(self, table):
-        return InsertWrapper(table, self.default_data_source)
+        return InsertWrapper(table, self._default_data_source)
 
     def wrapper(self):
-        return Wrapper(self.logical_delete)
+        return Wrapper(self._logical_delete)
 
     def raw(self, sql, args=()):
-        return self.default_data_source.get_executor().raw(sql, args)
+        return self._default_data_source.get_executor().raw(sql, args)
 
     def enable_logical_delete(self, logical_delete):
-        self.logical_delete = logical_delete
+        self._logical_delete = logical_delete
         return self
 
     def model(self, model_name):
-        return self.default_data_source.get_data_structure(model_name)
+        return self._default_data_source.get_data_structure(model_name)
+
+    def default_cache(self) -> LRUCache:
+        return self._default_cache
