2128bbfe4e681fd848f98c68e0364ad458db67b9
叶浩 <77729052@qq.com>
2025-01-10 14:31:54 +0800

    fix: verify token

--- seal/router/router.py
+++ seal/router/router.py
@@ -17,15 +17,25 @@ from ..model import Response as ResponseModel
 async def verify_token(request: Request = Request):
     if request.method == 'OPTIONS':
         return None
+
+    ignore = False
     path = request.url.path
     urls = configurator.get_config('seal', 'authorization', 'excludes')
     for url in urls:
         # * -> [a-zA-Z0-9_\-]*, ** -> .*
         url = url.replace('*', '[a-zA-Z0-9_\-]*').replace('**', '.*')
         if re.match(url, path):
-            return None
+            ignore = True
+            break
+
     try:
-        payload = jwt.decode(request.headers['Authorization'],
+        token = request.headers.get('Authorization', None)
+        if token is None or token == '':
+            if ignore:
+                return None
+            raise HTTPException(status_code=401, detail="Token is required")
+
+        payload = jwt.decode(token,
                              configurator.get_config('seal', 'authorization', 'jwt_key'),
                              algorithms=["HS256"])
         web_context.get().set_uid(payload['uid'])
